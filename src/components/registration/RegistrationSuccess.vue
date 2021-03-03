24f194c90c6d5cd7c6a37aaf3239a25b108c7acf
Leon2ev <jevgeni.glazunov@gmail.com>
2021-03-03 18:47:31 +0100

    restyle component using template

--- src/components/registration/RegistrationSuccess.vue
+++ src/components/registration/RegistrationSuccess.vue
@@ -1,42 +1,26 @@
 <template>
-  <div>
-    <h1 align="center">
+  <home-view-template>
+    <div slot="title">
       {{ registrationSuccessMsg }}
-    </h1>
-    <br />
-    <p align="center">
+    </div>
+    <div slot="description">
       Now you can Sign In to
-      <a
-        href="https://domain.cioty.com"
-        class="text-decoration-none synxpass-link"
-      >
+      <a href="https://domain.cioty.com" class="text-decoration-none">
         CIOTY.com
       </a>
-    </p>
-  </div>
+    </div>
+  </home-view-template>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import HomeViewTemplate from "../HomeViewTemplate";
 export default {
   computed: {
     ...mapState("registration", ["registrationSuccessMsg"])
+  },
+  components: {
+    HomeViewTemplate
   }
 };
 </script>
-
-<style scoped>
-h1 {
-  font-size: 30px;
-  font-weight: 300;
-  color: #ffffff;
-}
-p {
-  font-size: 16px;
-  font-weight: 300;
-  color: #ffffff;
-}
-.synxpass-link {
-  color: #dd5745;
-}
-</style>
