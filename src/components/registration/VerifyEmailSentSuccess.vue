d3b8b118f1a79fa5b702980c49a39cf2a1912371
Leon2ev <jevgeni.glazunov@gmail.com>
2021-03-04 12:22:24 +0100

    Update email sent success using template

--- src/components/registration/VerifyEmailSentSuccess.vue
+++ src/components/registration/VerifyEmailSentSuccess.vue
@@ -1,34 +1,24 @@
 <template>
-  <div>
-    <h1 align="center">
+  <home-view-template>
+    <div slot="title">
       {{ verifyEmailSentSuccessMsg }}
-    </h1>
-    <br />
-    <p align="center">
+    </div>
+    <div slot="description">
       Please click on the link that has just been sent to your email account to
       continue the registration proccess
-    </p>
-  </div>
+    </div>
+  </home-view-template>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import HomeViewTemplate from "../HomeViewTemplate";
 export default {
   computed: {
     ...mapState("registration", ["verifyEmailSentSuccessMsg"])
+  },
+  components: {
+    HomeViewTemplate
   }
 };
 </script>
-
-<style scoped>
-h1 {
-  font-size: 30px;
-  font-weight: 300;
-  color: #ffffff;
-}
-p {
-  font-size: 16px;
-  font-weight: 300;
-  color: #ffffff;
-}
-</style>
